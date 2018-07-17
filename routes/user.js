a3f5c9c2ffba8693af50efca9aa607027933eb2f
Ruben <rubenvilchesperez1985@hotmail.com>
2018-07-17 09:05:25 +0200

    profile, edit profile sin terminar

--- routes/user.js
+++ routes/user.js
@@ -7,4 +7,19 @@ userRoutes.get('/profile', (req, res, next) => {
   res.render('user/profile');
 });
 
+/* GET Edit the user in DB */
+userRoutes.get('/profile/edit', (req, res, next) => {
+    User.findById(req.params.id).then(user => {
+      res.render('/edit',{user});;
+    })
+  });
+
+/* POST Edit the user in DB */
+userRoutes.post('/profile/edit/:id', (req,res) => {
+    const { name, lastname, picture, isBabysitter, phone, email, password } = req.body;
+    User.findByIdAndUpdate(req.params.id,{ name, lastname, picture, isBabysitter, phone, email, password })
+        .then( user => {
+          res.redirect('/profile/:id')
+        })
+    })
 module.exports = userRoutes;
